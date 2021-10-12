3423be7bbb53bedcdd8bb612f550599f9d431986
songerard <songerard@gmail.com>
2021-10-12 11:51:41 +0800

    feat: add bootstrap and modify main.hbs

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const exphbs = require('express-handlebars')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
+// use static files
+app.use(express.static('public'))
+
 // set port
 const port = 3000
 
