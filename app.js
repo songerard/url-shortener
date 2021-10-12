2109cf1214967c4b88e87024d1a9abf1196d5e66
songerard <songerard@gmail.com>
2021-10-12 11:12:54 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,22 @@
+// require express
+const express = require('express')
+const app = express()
+
+// require handlebars
+const exphbs = require('express-handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+// set port
+const port = 3000
+
+// listen to localhost
+app.listen(port, () => {
+  console.log(`Express is listening to http://localhost:${port}`)
+})
+
+// ******** set route ******** //
+// index page
+app.get('/', (req, res) => {
+  res.render('index')
+})
